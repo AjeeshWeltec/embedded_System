4ce8bcd92ac6024d09f88dfa2e87929e9f0d7525
Ajeesh <ajeesh.chandran31@gmail.com>
2020-07-09 15:46:38 +1200

    Home screen + mode screen

--- Logical_Analyser_ES_CoronaVersion/src/main.cpp
+++ Logical_Analyser_ES_CoronaVersion/src/main.cpp
@@ -14,47 +14,85 @@
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
 
-#define LOGO_HEIGHT   16
-#define LOGO_WIDTH    16
+//#define LOGO_HEIGHT   16
+//#define LOGO_WIDTH    16
+#define DACpin A14
+#define ADCpin 14
+#define Buttonpin 12
 
-static const unsigned char PROGMEM logo_bmp[] =
-{ B00000000, B11000000,
-  B00000001, B11000000,
-  B00000001, B11000000,
-  B00000011, B11100000,
-  B11110011, B11100000,
-  B11111110, B11111000,
-  B01111110, B11111111,
-  B00110011, B10011111,
-  B00011111, B11111100,
-  B00001101, B01110000,
-  B00011011, B10100000,
-  B00111111, B11100000,
-  B00111111, B11110000,
-  B01111100, B11110000,
-  B01110000, B01110000,
-  B00000000, B00110000 };
- void testwelcometext(void);
-  void setup() {
-  Serial.begin(115200);
-if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
-    Serial.print("SSD1306 allocation failed");
-    for(;;); // Don't proceed, loop forever
-  }
-testwelcometext();    // Draw welcome text
-  }
-  void loop(){  
-  }
-  void testwelcometext(void) {
-  display.clearDisplay();
+uint8_t ButtonMode =0;
+int8_t ButtonPress =0;
+uint8_t previousButtoncount =0;
+uint8_t currentButtoncount =0;
+uint32_t debouce_delay =0;
 
-  display.setTextSize(2); // Draw 2X-scale text
-  display.setTextColor(WHITE,BLACK);
-  display.setCursor(20, 20);//displaying at coordinates 20-x,20-y
-  display.println("WELCOME");
-  display.setTextSize(1);//font size 1X- scale
-  display.println("ES -Logical Analyser");
-  display.display();      // Show initial text
-  delay(100);
+void Button(); 
+void HomeScreen();
+void ModeSelection();
 
+void setup() 
+  {
+   analogWriteResolution(8);
+   analogReadResolution(8);
+   Serial.begin(9600);
+   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) 
+    { 
+     Serial.print("DISPLAY-ERORR!!!");
+     for(;;); // Don't proceed, loop forever
+    }
+   pinMode(Buttonpin, INPUT_PULLUP);
+   //attachInterrupt(Buttonpin, Button, FALLING);
+  }
+void loop(){  
+ if(ButtonPress==0){
+   HomeScreen();
+  }
+  if(!digitalRead(Buttonpin)){
+  ModeSelection();
+  ButtonPress = 1;  
+ }
+}
+
+void HomeScreen() {
+ display.clearDisplay();
+ display.setTextSize(2); // Draw 2X-scale text
+ display.setTextColor(WHITE);
+ display.setCursor(20, 20);//displaying at coordinates 20-x,20-y
+ display.println("WELCOME");
+ display.setTextSize(1);//font size 1X- scale
+ display.println("ES -LOGICAL ANALYSER");
+ display.setCursor(65, 55);
+ display.setTextSize(.5);//font size 1X- scale
+ display.println("-AJEESH");
+ display.display();
+ ButtonPress = 1;
+}
+
+void ModeSelection(){
+ display.clearDisplay();
+ display.setTextSize(1.5);
+ display.setTextColor(WHITE);
+ display.setCursor(5,5);
+ display.println("> OSCILLOSCOPE");
+ display.setTextSize(1.5);
+ display.setTextColor(WHITE);
+ display.setCursor(5,15);
+ display.println("> FUNCTION GENERATOR");
+ display.setTextSize(1);
+ display.setTextColor(WHITE);
+ display.setCursor(15,25);
+ display.println("- SINE");
+ display.setTextSize(1);
+ display.setTextColor(WHITE);
+ display.setCursor(15,35);
+ display.println("- TRIANGLE");
+ display.setTextSize(1);
+ display.setTextColor(WHITE);
+ display.setCursor(15,45);
+ display.println("- SQUARE");
+ display.setTextSize(1.5);
+ display.setTextColor(WHITE);
+ display.setCursor(5,55);
+ display.println("> LOGIC ANALYSER"); 
+ display.display();
 }
